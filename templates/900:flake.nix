31f342386be4ecca70942447312f29c77620e8fa
Dietrich Daroch <Dietrich@Daroch.me>
2025-11-25 15:57:51 -0300

    Nix: Add flake.nix

---
+++ templates/900:flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "${1:Description}";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      ...,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              ${2:packages}
+            ];
+          };
+        }
+      );
+    };
+}
